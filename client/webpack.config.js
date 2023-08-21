3ea3322bc778e429c87c4c586a01a46077ce1b48
Chase Miller <chasehmiller97@gmail.com>
2023-08-21 13:00:37 -0400

    addition to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,47 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+        // webpack that generates the html file and injects into our bundles
+      }),
+
+      new InjectManifest({
+        swSRC: './src-sw.js',
+        swDest: 'src-sw.js',
+        // injecting the custom service worker
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Text Editor with a varierty of options",
+        background_color: "#225ca3",
+        theme_color: "225ca3",
+        start_url: "/",
+        publicpath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+            // creating a manifest.json file, line 32 - line 46
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+
+        }
         
       ],
     },
